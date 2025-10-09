5bc46ed5e86f4d945ab1c79edc922ea15e7e3802
coral25 <101058665+coral25@users.noreply.github.com>
2025-10-09 11:30:22 +0200

    fixed event

--- app/Events/MoveProcessed.php
+++ app/Events/MoveProcessed.php
@@ -1,24 +1,43 @@
 <?php
+
 namespace App\Events;
 
 use App\Models\Game;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MoveProcessed implements ShouldBroadcast
+class MoveProcessed implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public int $gameId;
+    public string $fen;
+    public string $message;
 
     /**
      * Create a new event instance.
      */
-    public function __construct(public Game $game, public string $result)
+    public function __construct(Game $game, string $result)
     {
-        $game->load('white_player', 'black_player', 'moves');
+        $this->gameId = $game->id;
+        $this->fen = $game->fen;
+        $this->message = $result;
+    }
+
+    /**
+     * Get the data to broadcast.
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'gameId' => $this->gameId,
+            'fen' => $this->fen,
+            'message' => $this->message,
+        ];
     }
 
     /**
